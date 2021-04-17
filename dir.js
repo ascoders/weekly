2e966d717dab89332a4cca51c63f6b5735e731e7
ascoders <576625322@qq.com>
2021-04-17 17:25:42 +0800

    update readme

--- dir.js
+++ dir.js
@@ -3,5 +3,6 @@ const fs = require('fs')
 const readDir = fs.readdirSync('./编译原理');
 
 readDir.forEach(dirName=>{
-  console.log(`- [${dirName}](./${encodeURI('编译原理')}/${encodeURI(dirName)})`)
+  // console.log(`- [${dirName}](./${encodeURI('编译原理')}/${encodeURI(dirName)})`)
+  console.log(`- [${dirName}](./编译原理/${dirName})`)
 })
